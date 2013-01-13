3d223bb1f7ca9c73e38849af42683ef94f1bd4a7
Loris Fichera <loris.fichera@gmail.com>
2013-01-13 06:18:48 -0500

    added first test case

--- src/game_of_life.erl
+++ src/game_of_life.erl
@@ -1,11 +1,33 @@
 -module(game_of_life).
 
+-export([life/1]).
+
+life(AliveNeighbours) when AliveNeighbours < 2 -> dead;
+life(AliveNeighbours) -> nil.
 
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
 should_always_pass_test() ->
-    ?assert(false).
+    ?assert(true).
+
+%% Any live cell with fewer than two live neighbours dies,
+%%  as if caused by under-population.
+should_die_when_underpopulated_test() ->
+    State1 = life(1),
+    ?assertEqual(dead, State1),
+    
+    State0 = life(0),
+    ?assertEqual(dead, State0).
+
+
+%% Any live cell with two or three live neighbours lives on to the next generation.
+
+%% Any live cell with more than three live neighbours dies,
+%%  as if by overcrowding.
+
+%% Any dead cell with exactly three live neighbours becomes a live cell,
+%%  as if by reproduction.
 
 -endif.
